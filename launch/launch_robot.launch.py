b521f3e5e6c567796ca7820f95aa56dc9e36086a
BG <bhaveshgandhi843@gamil.com>
2024-04-22 22:47:51 +0530

    Hardware Interface ros2_control

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -4,9 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
+from launch.substitutions import Command
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substituions include Command
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
 
@@ -27,10 +27,6 @@ def generate_launch_description():
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false','use_ros2_control':'true'}.items()
     )
-    
-
-
-
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -7,7 +7,8 @@ from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
-#import xacro
+import xacro
+
 
 def generate_launch_description():
 
@@ -18,8 +19,8 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('hosbot_pkg'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    #robot_description_config = xacro.process_file(xacro_file).toxml()
-    robot_description_config = Command(['xacro',xacro_file,'use_ros2_control:=',use_ros2_control,'use_ros2_control:=',use_sim_time])
+    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
@@ -30,14 +31,6 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_joint_state_publisher = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        output='screen',
-        parameters=[params]
-    )
 
     # Launch!
     return LaunchDescription([
@@ -45,11 +38,10 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-            DeclareLaunchArgument(
+        DeclareLaunchArgument(
             'use_ros2_control',
             default_value='true',
             description='Use ros2_control if true'),
 
-        node_robot_state_publisher,
-        node_joint_state_publisher
+        node_robot_state_publisher
     ])
